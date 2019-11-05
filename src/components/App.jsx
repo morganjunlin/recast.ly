673fdcd80f7fa2d9b611fff0fb22713a3d0b7c99
Morgan Lin <morganjunlin@gmail.com>
2019-11-04 22:30:06 -0800

    [feat] converted to stateful class component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,45 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
+import Search from './Search.js'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: props.exampleVideoData,
+      video: props.exampleVideoData[0]
+    }
+
+    this.handleVideoClick = this.handleVideoClick.bind(this);
+  }
+
+  handleVideoClick(idx) {
+    this.setState({
+      video: this.props.exampleVideoData[idx]
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <Search />
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={this.state.video} />
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={this.state.videos} handleVideoClick={this.handleVideoClick} />
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
-);
+    )
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
